413c67fe5e45be2e5d08fa645b58a88fd39db9c6
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-09-13 15:11:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,52 +37,52 @@ function draw() {
    drawSprites();
    
   
-var select_sprites = Math(random(1,3));
+// var select_sprites = Math(random(1,3));
 
-var select_sprites = Math.random(random(1,3));
+// var select_sprites = Math.random(random(1,3));
 
-var select_sprites = Math.round(1,3);
+// var select_sprites = Math.round(1,3);
 
-var select_sprites = Math.round(random(1,3));
+// var select_sprites = Math.round(random(1,3));
 
   
-  if (frameCount % 50 == 0) {
-    if (select_sprites == 1) {
-      createApples();
-    } else if (select_sprites == 2) {
-      createOrange();
-    }else {
-      createRed();
-    }
-  }
+  // if (frameCount % 50 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
 
-  if (frameCount % 80 == 0) {
-    if (select_sprites == 1) {
-      createApples();
-    } else if (select_sprites == 2) {
-      createOrange();
-    }
-  }
+  // if (frameCount % 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }
+  // }
 
-  if (frameCount / 80 == 0) {
-    if (select_sprites == 1) {
-      createApples();
-    } else if (select_sprites == 2) {
-      createOrange();
-    }else {
-      createRed();
-    }
-  }
+  // if (frameCount / 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
 
-  if (frameCount % 80 = 0) {
-    if (select_sprites == 1) {
-      createApples();
-    } else if (select_sprites == 2) {
-      createOrange();
-    }else {
-      createRed();
-    }
-  }
+  // if (frameCount % 80 = 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
 
 
 
